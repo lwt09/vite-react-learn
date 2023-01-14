f3f6ef30aa57003ff2641f8aafc029cc97ab3b1d
lwt09 <1102757555@qq.com>
2023-01-14 14:30:58 +0800

    feat: 测试lint-staged2

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import './header.scss';
 
 export default function App() {
-  console.log('test');
+  console.log('test2');
   return <div className='header'>我是header</div>;
 }
