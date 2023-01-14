1d23a217c1cabaffdf04473a160a2454717c4af5
lwt09 <1102757555@qq.com>
2023-01-14 16:51:58 +0800

    feat: 修复lint-staged loop打印问题

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import './header.scss';
 
 export default function App() {
-  console.log('test4');
+  console.log('test5');
   return <div className='header'>我是header</div>;
 }
