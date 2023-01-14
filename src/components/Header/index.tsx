dcdc3a7e506e1eb6617dabee15be5ac7ad5ffd12
lwt09 <1102757555@qq.com>
2023-01-14 15:30:07 +0800

    index on master: ad5a02c feat: 测试lint-staged3

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,6 @@ import React from 'react';
 import './header.scss';
 
 export default function App() {
-  console.log('test3');
+  console.log('test4');
   return <div className='header'>我是header</div>;
 }
