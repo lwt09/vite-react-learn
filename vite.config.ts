fc478224663e183d60b4283b82af887568ceaa7a
lwt09 <1102757555@qq.com>
2023-01-16 15:08:07 +0800

    feat: 压缩图片 + 处理dist被lint-staged

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,38 @@ import viteEslint from 'vite-plugin-eslint';
 // svg
 import svgr from 'vite-plugin-svgr';
 
+// 压缩图片
+import viteImagemin from 'vite-plugin-imagemin';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteEslint(), svgr()],
+  plugins: [
+    react(),
+    viteEslint(),
+    svgr(),
+    viteImagemin({
+      // 无损压缩配置，无损压缩下图片质量不会变差
+      optipng: {
+        optimizationLevel: 7,
+      },
+      // 有损压缩配置，有损压缩下图片质量可能会变差
+      pngquant: {
+        quality: [0.8, 0.9],
+      },
+      // svg 优化
+      svgo: {
+        plugins: [
+          {
+            name: 'removeViewBox',
+          },
+          {
+            name: 'removeEmptyAttrs',
+            active: false,
+          },
+        ],
+      },
+    }),
+  ],
   css: {
     postcss: {
       plugins: [postCssPresetEnv()],
